bc08d65d5937b7b481da2a50b9a903462039fe71
Margarita <34695872+MargaritaRay@users.noreply.github.com>
2018-02-22 21:02:14 +0300

    Update index1.2.php

--- index1.2.php
+++ index1.2.php
@@ -4,7 +4,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-/*
+
 if (isset($_GET['x'])){
 	$x = $_GET['x'];
 }else{
@@ -21,52 +21,32 @@ if (isset($_GET['arithmetic'])){
 	$oper = $_GET['arithmetic'];
 }else{
 	$oper = null;
-}*/
+}
 
-//если переменная была установленна, передаем ее, если не была установленна, передаем null
-
-if (isset($_GET['x']) || isset($_GET['y']) || isset($_GET['arithmetic'])){
-	$x = $_GET['x'];
-	$y = $_GET['y'];
-	$oper = $_GET['arithmetic'];
-}else{
-	$x = null;
-	$y = null;
-	$oper = null;
-};
-
-//пришлось это все записать в функцию поскольку на $z тоже нотик жаловался 
 
 
 function calc($x, $y, $oper){
-	if($oper == '+'){
-		if($x == null || $y == null || $oper == null){
-			return 'Заполните все поля формы';
-		}else{
+	if($x == null || $y == null || $oper == null){
+		return 'Заполните все поля формы';
+	}else{
+		if($oper == '+'){
 			return  $x + $y;
-		}
-	}elseif ($oper == '-'){
-		if($x == null || $y == null || $oper == null){
-			return 'Заполните все поля формы';
-		}else{
+		}elseif ($oper == '-'){
 			return  $x - $y;
-		}
-	} elseif ($oper == '*'){
-		if($x == null || $y == null || $oper == null){
-			return 'Заполните все поля формы';
-		}else{
+		} elseif ($oper == '*'){
 			return  $x * $y;
-		}
-	} elseif ($oper == '/'){
-		if($x == null || $y == null || $oper == null){
-			return 'Заполните все поля формы';
-		}else{
-			return  $x / $y;
+		} elseif ($oper == '/'){
+			if($x == 0 || $y == 0 ){
+				return 'На 0 делить нельзя!';
+			}else{
+				return  $x / $y;
+			}
 		}
 	}
 }
 
-$z = cal($x, $y, $oper);
+$z = calc($x, $y, $oper);
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -79,7 +59,7 @@ $z = cal($x, $y, $oper);
 </head>
 <body>
 
-<form action="index1.2.php" method="get">
+<form action=" " method="get">
 	<label for="">
 		<!--Выводим в значение input x введенные ранее пользователем данные-->
 		<input type="number" name="x" value="<?php echo $x; ?>">
